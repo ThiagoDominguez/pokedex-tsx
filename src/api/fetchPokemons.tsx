0a8b8df586c80626153fc9f8ffdc3b1e042fa20c
ThiagoDominguez <credgeyt@gmail.com>
2024-01-31 20:35:28 -0300

    feat(api): update URL and maping to the API
    update URL and maping the each one of the all pokemons geted to the API

--- src/api/fetchPokemons.tsx
+++ src/api/fetchPokemons.tsx
@@ -1,25 +1,31 @@
 // NOTE API 2 https://unpkg.com/pokemon@2.3.6/data/es.json
 
-export const fetchPokemons = async () => {
-  const response = await fetch("https://unpkg.com/pokemon@2.3.6/data/es.json");
+import { Pokemon } from "../types/types";
+import { formatName } from "../utils/utils";
+
+export async function fetchPokemons(): Promise<Pokemon[]> {
+  const response = await fetch(
+    "https://unpkg.com/pokemons@1.1.0/pokemons.json"
+  );
 
   if (!response.ok) {
     throw new Error("Failed to fetch pokemons");
   }
 
   const results = await response.json();
-  console.log(results);
 
-  const pokemons = results.map((pokemon: any) => ({
+  const pokemons = results.results.map((pokemon: any) => ({
     name: pokemon.name,
     id: pokemon.national_number,
-    imgSrc: `https://img.pokemondb.net/sprites/black-white/anim/normal/${pokemon.name}.gif`,
+    imgSrc: `https://img.pokemondb.net/sprites/black-white/anim/normal/${formatName(
+      pokemon.name.toLowerCase()
+    )}.gif`,
   }));
 
-  const uniquePokemons = pokemons.filter(
+  const uniquePokemons = pokemons?.filter(
     (pokemon: any, index: number) =>
       pokemons.findIndex((other: any) => other.id === pokemon.id) === index
   );
 
   return uniquePokemons;
-};
+}
