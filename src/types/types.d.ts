1cc32d79e2e6f58efefc3c597b391d8fc18c72e2
ThiagoDominguez <credgeyt@gmail.com>
2024-01-31 10:19:13 -0300

    refactor: add types
    add types such as <Pokemon> and <PokemonDetails>

---
+++ src/types/types.d.ts
@@ -0,0 +1,13 @@
+export type Pokemon = {
+  id: number;
+  name: string;
+  imgSrc: string;
+};
+export type PokemonDetails = {
+  id: number;
+  name: string;
+  imgSrc: string;
+  hp: number;
+  attack: number;
+  defense: number;
+};
